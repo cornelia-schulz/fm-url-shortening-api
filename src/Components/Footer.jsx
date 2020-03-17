80eec2e89ef6fc66a71dc6ab8a071c986749357b
Cornelia Schulz <cschulz@nrl.com.au>
2020-03-17 18:37:02 +1100

    update footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,42 +1,52 @@
 import React from 'react';
+import { ReactComponent as Logo } from '../logo.svg';
 
 function Footer() {
   return (
     <footer className="footer container">
       <div className="footer-logo">
-        <img alt="logo" src="/images/logo.svg" />
+        <Logo />
       </div>
       <div className="footer-links">
         <div className="footer-link-list-wrapper">
-          <h3>Features</h3>
+          <h4>Features</h4>
           <ul className="footer-link-list">
-            <li>Link Shortening</li>
-            <li>Branded Links</li>
-            <li>Analytics</li>
+            <li><a href="/">Link Shortening</a></li>
+            <li><a href="/">Branded Links</a></li>
+            <li><a href="/">Analytics</a></li>
           </ul>
         </div>
         <div className="footer-link-list-wrapper">
-          <h3>Resources</h3>
+          <h4>Resources</h4>
           <ul className="footer-link-list">
-            <li>Link Shortening</li>
-            <li>Branded Links</li>
-            <li>Analytics</li>
+            <li><a href="/">Blog</a></li>
+            <li><a href="/">Developers</a></li>
+            <li><a href="/">Support</a></li>
           </ul>
         </div>
         <div className="footer-link-list-wrapper">
-          <h3>Company</h3>
+          <h4>Company</h4>
           <ul className="footer-link-list">
-            <li>Link Shortening</li>
-            <li>Branded Links</li>
-            <li>Analytics</li>
+            <li><a href="/">About</a></li>
+            <li><a href="/">Our Team</a></li>
+            <li><a href="/">Careers</a></li>
+            <li><a href="/">Contact</a></li>
           </ul>
         </div>
       </div>
       <div className="footer-social">
-        <img alt="facebook" src="/images/icon-facebook.svg" />
-        <img alt="twitter" src="/images/icon-twitter.svg" />
-        <img alt="pinterest" src="/images/icon-pinterest.svg" />
-        <img alt="instagram" src="/images/icon-instagram.svg" />
+        <a href="/">
+          <img alt="facebook" src="/images/icon-facebook.svg" />
+        </a>
+        <a href="/">
+          <img alt="twitter" src="/images/icon-twitter.svg" />
+        </a>
+        <a href="/">
+          <img alt="pinterest" src="/images/icon-pinterest.svg" />
+        </a>
+       <a href="/">
+         <img alt="instagram" src="/images/icon-instagram.svg" />
+       </a>
       </div>
     </footer>
   )
