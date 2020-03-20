697768efe57682ad09a954ac8a352b247439cd56
Cornelia Schulz <cschulz@nrl.com.au>
2020-03-20 17:39:22 +1100

    add blurb

--- src/Components/Blurb.jsx
+++ src/Components/Blurb.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 function Blurb() {
   return (
     <div className="blurb container">
-      <h1>More than just shorter links</h1>
-      <p>Build your brand's recognition and get detailed insights on how your links are performing</p>
-      <button className="btn-rounded">Get started</button>
+      <div className="blurb-wrapper">
+        <h1>More than just shorter links</h1>
+        <p>Build your brand's recognition and get detailed insights on how your links are performing</p>
+        <button className="btn-rounded">Get started</button>
+      </div>
     </div>
   )
 }
